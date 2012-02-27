d4b9687a6dec15b5c99b138aa2bf4935a336060b
Ben Grimes <bgrimes@gmail.com>
2012-02-27 11:07:10 -0500

    Added solarium to the project, created bootstrap file, updated readme

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Solr: Solr wrapper for FuelPHP
+ *
+ * @package    Solr
+ * @version    v0.0
+ * @author     Ben Grimes
+ * @license    MIT License
+ */
+
+
+require('solarium/library/Solarium/Autoloader.php');
+Solarium_Autoloader::register();
+
+/* End of file bootstrap.php */
---
+++ config/solarium.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Solr: Solr wrapper for FuelPHP
+ *
+ * @package    Solr
+ * @version    v0.0
+ * @author     Ben Grimes
+ * @license    MIT License
+ */
+
+return array(
+    // Server information
+    'default' => array(
+        'url'         => 'http://127.0.0.1',
+        'port'        => 8080,
+        'application' => 'default',
+    ),
+);
+
+/* End of file config/solr.php */
